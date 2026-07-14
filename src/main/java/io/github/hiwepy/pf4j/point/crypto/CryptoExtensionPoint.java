a326d3b0c0165e7ca3b30347ea470bc5d779e974
wandl-6A72h <hiwepy@gmail.com>
2026-07-14 15:57:50 +0800

    refactor: migrate JDK 8 PF4J extension

---
+++ src/main/java/io/github/hiwepy/pf4j/point/crypto/CryptoExtensionPoint.java
@@ -0,0 +1,18 @@
+package io.github.hiwepy.pf4j.point.crypto;
+
+import org.pf4j.ExtensionPoint;
+
+/**
+ * 加解密扩展点接口
+ *
+ * <p>定义加解密功能的扩展点，插件可以通过实现此接口提供自定义的加解密算法。
+ *
+ * @author <a href="https://github.com/partme-ai">PartMe.AI</a>
+ */
+public interface CryptoExtensionPoint extends ExtensionPoint {
+
+    String encrypt(String source, Object secretKey);
+
+    String decrypt(String source, Object secretKey);
+
+}
